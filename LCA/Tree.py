c6e4352cf11d84516ebefa6ae86727daba29a7cf
Nissi <nissimanjayil@gmail.com>
2019-09-20 16:30:18 +0100

    Change BinARY tREE

---
+++ LCA/Tree.py
@@ -0,0 +1,29 @@
+from Node import Node;
+
+
+class Tree:
+
+    def ___init___(self,root=none):
+        self.root = root
+
+    def LCA(self,root,p,q):
+
+        if not root or not p or not q:
+            return none
+
+        if root is p or root is q:
+            return root
+
+    return none
+
+
+def main():
+    n7 = Node(7)
+    n5 = Node()
+
+
+
+
+
+if __name__ == '__main__':
+    main()

20f20ca5e5453c21c2b02478ca82a8cb125837a5
Nissimol Aji <44730306+nissimanjayil@users.noreply.github.com>
2019-10-14 11:41:48 +0100

    Adding Graph and Edge Function

--- LCA/src/LCADAG.java
+++ LCA/src/LCADAG.java
@@ -1,4 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+//This stores the graphs edges
+class Edge{
+	int source, dest;
+	
+	public Edge(int source,int  dest){
+		this.source=source;
+		this.dest=dest;
+	}
+}
+class Graph{
+	List<List<Integer>> adjList = null;
+	
+	Graph(List<Edge>edges,int N){
+		adjList = new ArrayList<>(N);
+		
+		for(int i=0;i<N;i++){
+			adjList.add(i,new ArrayList<>());
+		}
+		
+		for(Edge edge:edges){
+			adjList.get(edge.source).add(edge.dest);
+		}
+	}
+}
 
 public class LCADAG {
+	
 
 }
